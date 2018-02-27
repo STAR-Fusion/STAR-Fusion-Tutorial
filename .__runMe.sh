be9f4bbca1d6885763c787fe18c15a36e7622066
brianjohnhaas <brianjohnhaas@gmail.com>
2018-02-27 20:48:52 +0000

    add

---
+++ .__runMe.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+set -ev
+
+# prep genome lib
+../FusionFilter/prep_genome_lib.pl --genome_fa minigenome.fa --gtf minigenome.gtf --fusion_annot_lib CTAT_HumanFusionLib.mini.dat.gz --annot_filter_rule AnnotFilterRule.pm
+
+# run CTAT fusion tools via the STAR-F entry point
+../STAR-Fusion --left_fq rnaseq_1.fastq.gz --right_fq rnaseq_2.fastq.gz --genome_lib_dir ctat_genome_lib_build_dir --FusionInspector validate --denovo_reconstruct
+
