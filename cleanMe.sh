078ae2f60a16140c37c74400f54ebdf8a92730fe
brianjohnhaas <brianjohnhaas@gmail.com>
2018-03-02 17:27:10 +0000

    adding cleaner script

---
+++ cleanMe.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+rm -rf ./ctat_genome_lib_build_dir
+
+rm -rf ./STAR-Fusion_outdir
+
+rm -rf ./__loc_chkpts
+
+rm -f ./ref_annot.cdsplus*
+rm -f ./ref_annot.cdna*
+rm -f ./pipeliner*
+
+rm -f ./Log.out
