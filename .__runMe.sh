3519a2891b5e38b7d82cb19ec4e8279df01c22c5
Brian Haas <brianjohnhaas@gmail.com>
2019-08-07 13:20:45 -0400

    update to auto launcher

--- .__runMe.sh
+++ .__runMe.sh
@@ -3,8 +3,8 @@
 set -ev
 
 # prep genome lib
-../FusionFilter/prep_genome_lib.pl --genome_fa minigenome.fa --gtf minigenome.gtf --fusion_annot_lib CTAT_HumanFusionLib.mini.dat.gz --annot_filter_rule AnnotFilterRule.pm
+../STAR-Fusion/ctat-genome-lib-builder/prep_genome_lib.pl --genome_fa minigenome.fa --gtf minigenome.gtf --fusion_annot_lib CTAT_HumanFusionLib.mini.dat.gz --annot_filter_rule AnnotFilterRule.pm
 
 # run CTAT fusion tools via the STAR-F entry point
-../STAR-Fusion --left_fq rnaseq_1.fastq.gz --right_fq rnaseq_2.fastq.gz --genome_lib_dir ctat_genome_lib_build_dir --FusionInspector validate --denovo_reconstruct
+../STAR-Fusion/STAR-Fusion --left_fq rnaseq_1.fastq.gz --right_fq rnaseq_2.fastq.gz --genome_lib_dir ctat_genome_lib_build_dir --FusionInspector validate --denovo_reconstruct
 
